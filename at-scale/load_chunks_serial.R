cb106269afb11cc4fe5c26252afbe6e0c1a9e108
Tim <tbarry2@andrew.cmu.edu>
2022-07-10 23:30:21 -0400

    use fread instead

--- at-scale/load_chunks_serial.R
+++ at-scale/load_chunks_serial.R
@@ -3,5 +3,12 @@ test_dir <- paste0(gasp_offsite, "raw/test")
 fs <- list.files(test_dir, full.names = TRUE)
 
 for (f in fs) {
-  x <- readr::read_delim(file = f, delim = " ", skip = 2, col_types = c("iii"))
+  print(paste0("Loading ", f))
+  # x <- readr::read_delim(file = f, delim = " ", skip = 2, col_types = c("iii"))
+  x <- data.table::fread(file = f,
+                         sep = " ",
+                         colClasses = c("integer", "integer", "integer"),
+                         skip = 2)
+  rm(x)
+  gc()
 }
